9ec328ebfe83d24ab18d37f89dc9a18d47914869
pengfei <pengfei@axbbaoxian.com>
2024-05-29 16:14:19 +0800

    fix: 拼接字段git

--- utils/config.js
+++ utils/config.js
@@ -3,9 +3,8 @@ config.accessKey = 'ubcXyKQWyJ88g-ks0SWxc-Ev-Bye24PD5iGhoQJB'; //七牛云Ak密�
 config.secretKey = 'R-2WsRj8YJOSQ690mq_I5wYv1QMebx0oveJ3GZ64'; //七牛云Sk密匙
 config.bucket = 'pallblogs'; //存储空间的名字
 //github远程仓库地址更新日志
-// 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
-config.githubUrl = ''
-config.selectgit = ''
+config.githubUrl = 'github_pat_11BAHSRNQ0X2eDZ3KdTCrs_'
+config.selectgit = 'nGgxc8JCMu8MMpr3kp20oL84HSCZb7ganL0WlstipbfESUALKQLdHdiIZ2S'
 
 config.url = 'http://rndznqlcz.hn-bkt.clouddn.com/'; //配置的域名
 
